ed9d9dc145fa553f9d05458a1a809203e3f7af17
Andy <anderzanovoleg@gmail.com>
2022-11-12 17:13:31 +0300

    user updating was done

--- controller/user_controller.py
+++ controller/user_controller.py
@@ -6,6 +6,7 @@ from entity.entities import UserEntity
 from factory import generate_session
 from model.user.user_add_model import UserAddModel
 from model.user.user_get_model import UserGetModel
+from model.user.user_update_model import UserUpdateModel
 from service import user_service
 
 user_controller = APIRouter()
@@ -14,3 +15,9 @@ user_controller = APIRouter()
 @user_controller.post("/api/user/")
 def register(user_data: UserAddModel, session: Session = Depends(generate_session)) -> UserGetModel:
     return user_service.add_user(user_data, session)
+
+
+@user_controller.put("/api/user/")
+def update_user(new_user_data: UserUpdateModel, session: Session = Depends(generate_session),
+                user: UserEntity = Depends(get_current_user)):
+    return user_service.update_user(new_user_data, user, session)
--- service/user_service.py
+++ service/user_service.py
@@ -1,10 +1,11 @@
 import bcrypt
-from sqlalchemy.orm import Session
+from sqlalchemy.orm import Session, Query
 
 from entity.entities import UserEntity
 from exception.exceptions import UserAlreadyExists
 from model.user.user_add_model import UserAddModel
 from model.user.user_get_model import UserGetModel
+from model.user.user_update_model import UserUpdateModel
 
 
 def add_user(user_data: UserAddModel, session: Session) -> UserGetModel:
@@ -28,3 +29,27 @@ def get_user_by_id(id: int, session: Session):
 
 def hash_password(password: str):
     return bcrypt.hashpw(password.encode(), bcrypt.gensalt()).decode()
+
+
+def update_user(new_user_data: UserUpdateModel, user: UserEntity, session: Session) -> UserGetModel:
+    new_user_data = convert_dto_to_dict(new_user_data)
+    if get_user_by_username(new_user_data['name'], session):
+        raise UserAlreadyExists("user with such name already exists")
+
+    req: Query = session.query(UserEntity).filter_by(id=user.id)
+
+    if new_user_data.get('password'):
+        new_user_data['password'] = hash_password(new_user_data['password'])
+
+    req.update(new_user_data)
+    session.commit()
+    return UserGetModel.to_model(user)
+
+
+def convert_dto_to_dict(dto):
+    dto_dict = dict()
+    for key, value in dto.dict().items():
+        if dto.dict()[key] is None:
+            continue
+        dto_dict[key] = value
+    return dto_dict
